94c9ab85deafb567c9a0134fd0747f245f531fcc
Christopher Villalta <va23003@esfe.agape.edu.sv>
2024-09-11 23:11:02 -0600

    updating Program.cs

--- ApiStore/Program.cs
+++ ApiStore/Program.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using ApiStore.DTOs;
 using ApiStore.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,41 +27,88 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing",
-    "Bracing",
-    "Chilly",
-    "Cool",
-    "Mild",
-    "Warm",
-    "Balmy",
-    "Hot",
-    "Sweltering",
-    "Scorching",
-};
+/* PRODUCTO */
 
 app.MapGet(
-        "/weatherforecast",
-        () =>
-        {
-            var forecast = Enumerable
-                .Range(1, 5)
-                .Select(index => new WeatherForecast(
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-                .ToArray();
-            return forecast;
-        }
-    )
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+    "/api/products/",
+    () =>
+    {
+        return "Lista de Productos";
+    }
+);
+
+app.MapGet(
+    "/api/products/{id}",
+    (int id) =>
+    {
+        return $"Buscando producto con el Id: {id}";
+    }
+);
+
+app.MapPost(
+    "/api/products/",
+    (ProductRequest product) =>
+    {
+        return $"Guardando producto con el  CategoriaId: {product.CategoriaId}";
+    }
+);
+
+app.MapPut(
+    "/api/products/{id}",
+    (int id, ProductRequest product) =>
+    {
+        return $"Modificando producto con el Id: {id}";
+    }
+);
+
+app.MapDelete(
+    "/api/products/{id}",
+    (int id) =>
+    {
+        return $"Eliminando producto con el Id: {id}";
+    }
+);
+
+/* CATEGORIA */
+
+app.MapGet(
+    "/api/categories/",
+    () =>
+    {
+        return "Lista de Categorias";
+    }
+);
+
+app.MapGet(
+    "/api/categories/{id}",
+    (int id) =>
+    {
+        return $"Buscando Categoria con el Id: {id}";
+    }
+);
+
+app.MapPost(
+    "/api/categories/",
+    (CategoryRequest category) =>
+    {
+        return $"Guardando categoria con el  CategoriaId: {category}";
+    }
+);
+
+app.MapPut(
+    "/api/categories/{id}",
+    (int id, CategoryRequest category) =>
+    {
+        return $"Modificando categoria con el Id: {id}";
+    }
+);
+
+app.MapDelete(
+    "/api/categories/{id}",
+    (int id) =>
+    {
+        return $"Eliminando categoria con el Id: {id}";
+    }
+);
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
