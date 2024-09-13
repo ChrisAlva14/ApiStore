d5c2ff6562068837ab77301e158dee2b55fa1554
MEGA SERVICES <lg23002@esfe.agape.edu.sv>
2024-09-12 19:52:31 -0600

    actualiozando

--- ApiStore/Services/Orders/IOrderServices.cs
+++ ApiStore/Services/Orders/IOrderServices.cs
@@ -1,6 +1,18 @@
-﻿namespace ApiStore.Services.orders
+﻿using ApiStore.DTOs;
+
+namespace ApiStore.Services.orders
 {
     public interface IOrderServices
     {
+        Task<int> PostOrder(OrderRequest order);
+
+        Task<List<OrderResponse>> GetOrder();
+
+        Task<OrderResponse> GetOrder(int orderId);
+
+        Task<int> PutOrder(int orderId, OrderRequest order);
+
+        Task<int> DeleteOrder(int orderId);
     }
 }
+
